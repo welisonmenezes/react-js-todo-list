31956504cafb6be0a87487af874a6316f7909091
welisonmenezes <welisonmenezes@hotmail.com>
2020-09-30 14:45:58 -0300

    Updates

---
+++ precache-manifest.42c681d194f66c6f0e454425aca41c24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c44fc7c3e73e037b258973c4557d975",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90be87a7cbee46170724",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4c424d534296eb160e64",
+    "url": "/static/js/2.fcc277c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fcc277c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90be87a7cbee46170724",
+    "url": "/static/js/main.c01cf375.chunk.js"
+  },
+  {
+    "revision": "a264bef6a524acf220ff",
+    "url": "/static/js/runtime-main.08f3cda5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

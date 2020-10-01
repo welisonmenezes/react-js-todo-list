c58f0a39e3d47a06f86bc9731c0bf9cda854ddd1
welisonmenezes <welisonmenezes@hotmail.com>
2020-10-01 18:33:26 -0300

    Implement dinamic status bar and percentage counter

--- src/components/Status.js
+++ src/components/Status.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
 import "./Status.css";
+import { AppContext } from "../contexts/AppContext";
 
 function Status() {
+    const [status, setStatus] = useState("0 of 0");
+    const [percentage, setPercentage] = useState(0);
+    const { todoItems } = useContext(AppContext);
+
+    useEffect(() => {
+        const completed = todoItems.filter((item) => item.completed);
+        const countAll = todoItems.length;
+        const countCompleted = completed.length;
+        setPercentage(100 / (countAll / countCompleted));
+        setStatus(`${completed.length} of ${todoItems.length}`);
+    }, [todoItems]);
+
     return (
         <div className="status">
-            <span>1 de 1</span>
+            <span>{status}</span>
             <div>
-                <div className="status-bar"></div>
+                <div
+                    className="status-bar"
+                    style={{ width: `${percentage}%` }}
+                ></div>
             </div>
         </div>
     );
